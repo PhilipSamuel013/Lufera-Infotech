abf425f4f13c47e0ecfb1c0e374a3b0d2e4ba564
ramesh-lufera <ramesh@lufera.in>
2025-04-22 10:31:51 +0530

    add new website page

--- website.php
+++ website.php
@@ -140,9 +140,9 @@
                 </form>
 
                 <div class="add-button">
-                    <form action="https://luferatech.com/package/" target="_blank" method="get">
-                    <button type="submit">+ Add New Website</button>
-                    </form>
+                    <a href="add-website.php">    
+                        <button>+ Add New Website</button>
+                    </a>
                 </div>
                 </div>
 
